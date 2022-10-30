4970cc1e25321755f1b21df18571b3fed93b5aa9
KonstantinVasiliev <grafit21@mail.ru>
2022-10-30 11:55:02 +0500

    save Task29

---
+++ Task29/Program.cs
@@ -0,0 +1,28 @@
+﻿
+int[] array = new int[8];
+
+void Fullarray(int[] collection)
+{
+    int lenght = collection.Length;
+    int index = 0;
+     while (index < lenght)
+     {
+        collection[index] = new Random().Next(-10, 10);
+        index++;
+     }
+}
+
+void Printarray(int[] col)
+{
+    int count = col.Length;
+    int pos = 0;
+    while (pos < count)
+    {
+        Console.WriteLine(col[pos]);
+        pos++;
+    }
+}
+
+Fullarray(array);
+Printarray(array);
+
