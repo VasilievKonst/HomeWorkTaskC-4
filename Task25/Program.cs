733e7a652973777d232bf2ac1b1cb2728db9c40d
KonstantinVasiliev <grafit21@mail.ru>
2022-10-30 10:32:59 +0500

    save task25

---
+++ Task25/Program.cs
@@ -0,0 +1,22 @@
+﻿// Ввод значений чисел A B
+
+double GetAB(string text)
+{
+Console.Write(text + ":");
+double value = Convert.ToDouble(Console.ReadLine());
+return value;
+}
+double A = GetAB("Введите значение числа А");
+double B = GetAB("Введите значение числа В");
+
+double count = 1;
+double resault = A;
+
+while (count < B)
+{
+    resault *= A;
+    count++ ;
+}
+
+Console.Write("Результатом возведения А=" +A+ ", в степень В=" +B+ ", будет являться число: ");
+Console.WriteLine(resault);
