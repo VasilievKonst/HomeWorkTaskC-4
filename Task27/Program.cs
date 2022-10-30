d7332174e013913cd8d49ee26f632d4a04f107fc
KonstantinVasiliev <grafit21@mail.ru>
2022-10-30 13:36:26 +0500

    save Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число: ");
+int x = int.Parse(Console.ReadLine());
+int sum = 0;
+string s = x.ToString();
+for (int i = s.Length; i>=0; i--)
+{
+    sum+=x/(int)Math.Pow(10, i);
+    x = x%(int)Math.Pow(10, i);
+}
+Console.Write("Сумма цифр числа составила: ");
+Console.WriteLine(sum);
+
+
+
+
